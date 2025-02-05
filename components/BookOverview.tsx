e3c54b8cc98ddd1765772a8b4d923f708119e666
Karl George <karlgeorge87@gmail.com>
2025-02-05 13:58:35 +0100

    Fix hero image

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -49,25 +49,25 @@ function BookOverview({
           <Image src={'/icons/book.svg'} alt='book' width={20} height={20} />
           <p className='font-bebas-neue text-xl text-dark-100'>Borrow</p>
         </Button>
+      </div>
 
-        {/* Book Cover Image */}
-        <div className='relative flex flex-1 justify-center'>
-          <div className='relative'>
+      {/* Book Cover Image */}
+      <div className='relative flex flex-1 justify-center'>
+        <div className='relative'>
+          <BookCover
+            variant='wide'
+            className='z-10'
+            coverColor={coverColor}
+            coverUrl={coverUrl}
+          />
+
+          {/* Second rotated Book Cover image */}
+          <div className='absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden'>
             <BookCover
               variant='wide'
-              className='z-10'
               coverColor={coverColor}
               coverUrl={coverUrl}
             />
-
-            {/* Second rotated Book Cover image */}
-            <div className='absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden'>
-              <BookCover
-                variant='wide'
-                coverColor={coverColor}
-                coverUrl={coverUrl}
-              />
-            </div>
           </div>
         </div>
       </div>
