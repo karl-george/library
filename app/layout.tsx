9924fa49b4652b37e78546a9c3ce565cc0777d9c
Karl George <karlgeorge87@gmail.com>
2025-02-10 13:59:54 +0100

    Add Session Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css';
 import localFont from 'next/font/local';
 import { ReactNode } from 'react';
 import { Toaster } from '@/components/ui/toaster';
+import { SessionProvider } from 'next-auth/react';
+import { auth } from '@/auth';
 
 // Setup local fonts with styling
 const ibmPlexSans = localFont({
@@ -30,15 +32,19 @@ export const metadata: Metadata = {
   description: 'BookWise is a book borrowing website solution',
 };
 
-const RootLayout = ({ children }: { children: ReactNode }) => {
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+  const session = await auth();
+
   return (
     <html lang='en'>
-      <body
-        className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
-      >
-        {children}
-        <Toaster />
-      </body>
+      <SessionProvider session={session}>
+        <body
+          className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
+        >
+          {children}
+          <Toaster />
+        </body>
+      </SessionProvider>
     </html>
   );
 };
