cee0f5b4f69d626cc30f96e612a4eb960a818fc1
Karl George <karlgeorge87@gmail.com>
2025-02-10 14:03:51 +0100

    Add redirect if user not signed in

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,16 @@
+import { auth } from '@/auth';
 import Header from '@/components/Header';
+import { redirect } from 'next/navigation';
 import React, { ReactNode } from 'react';
 
-const layout = ({ children }: { children: ReactNode }) => {
+const layout = async ({ children }: { children: ReactNode }) => {
   // Setup the nav bar for all required routes. Include children to pass
   // the pages contents
+
+  // Check if session doesn't exist to redirect
+  const session = await auth();
+  if (!session) redirect('/sign-in');
+
   return (
     <main className='root-container'>
       <div className='mx-auto max-w-7xl'>
