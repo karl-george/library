fc165715a8ef99c752a387cd86add7167ea1232c
Karl George <karlgeorge87@gmail.com>
2025-02-11 12:36:50 +0100

    Update users last activity date

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,10 @@
 import { auth } from '@/auth';
 import Header from '@/components/Header';
+import { db } from '@/database/drizzle';
+import { users } from '@/database/schema';
+import { eq } from 'drizzle-orm';
 import { redirect } from 'next/navigation';
+import { after } from 'next/server';
 import React, { ReactNode } from 'react';
 
 const layout = async ({ children }: { children: ReactNode }) => {
@@ -10,6 +14,15 @@ const layout = async ({ children }: { children: ReactNode }) => {
   // Check if session doesn't exist to redirect
   const session = await auth();
   if (!session) redirect('/sign-in');
+
+  after(async () => {
+    if (!session?.user?.id) return;
+
+    await db
+      .update(users)
+      .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+      .where(eq(users.id, session?.user.id));
+  });
 
   return (
     <main className='root-container'>
